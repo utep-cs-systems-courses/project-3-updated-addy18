c01d5d9808ca5175e27fbb670794e3b65c662983
Addy <amgalvan@miners.utep.edu>
2020-12-05 11:45:16 -0700

    commiting just in case

--- project/interrupt_handler.c
+++ project/interrupt_handler.c
@@ -1,7 +1,7 @@
 #include <msp430.h>
 #include "switches.h"
 
-//switch on P2
+//switches on P2
 void __interrupt_vec(PORT2_VECTOR) Port_2(){
   if(P2IFG & SWITCHES){  //button cause an interrupt?
     P2IFG &= ~SWITCHES;  // clear pending sw interrupt
