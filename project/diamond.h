f8fb6235de2c39ee36cdc5d88db9bdb863a91f52
Addy <amgalvan@miners.utep.edu>
2020-12-05 12:16:44 -0700

    add diamond.h

---
+++ project/diamond.h
@@ -0,0 +1,10 @@
+#ifndef diamond_included
+#define diamond_included
+
+#include "lcddraw.h"
+#include "lcdtypes.h"
+#include "lcdutils.h"
+
+int compare(u_char a, u_char b);
+void draw(u_char center, u_int state);
+#endif
