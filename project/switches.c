046aafa4d34356d75d5a8e61282003c53139a5ea
Addy <amgalvan@miners.utep.edu>
2020-12-09 01:34:43 -0700

    updated if statement for switches

--- project/switches.c
+++ project/switches.c
@@ -2,11 +2,8 @@
 #include "switches.h"
 #include "led.h"
 #include "State_Machine.h"
-#include "buzzer.h"
-#include "libTimer.h"
 
 char switch_state_down, switch_state_changed, s1, s2, s3, s4;
-//current_state; //effectively boolean
 
 static char switch_update_interrupt_sense(){
   char p2val = P2IN;
@@ -37,25 +34,15 @@ void switch_interrupt_handler(){
   if(s1){ // switch 1 pressed
     switch_state_down = s1;
     switch_state_changed = 1;
-    led_update();
-    song1();
   }else if(s2){ // switch 2 pressed
     switch_state_down = s2;
     switch_state_changed = 2;
-    led_update();
-    song2();
   }else if(s3){ // switch 3 pressed
     switch_state_down = s3;
     switch_state_changed = 3;
-    song3();
   }else if(s4){ // switch 4 pressed 
     switch_state_down = s4;
     switch_state_changed = 4;
-    //led_update();
-    state4();
-    song4();
   }
-  switch_state_changed = 1;
-  led_update();
 }
   
