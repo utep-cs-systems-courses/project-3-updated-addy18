fdb2b2a2c361ce7dbe4cfe186f650ded313121c8
Addy <amgalvan@miners.utep.edu>
2020-12-09 04:32:13 -0700

    created led files from project 2

---
+++ src/led.h
@@ -0,0 +1,15 @@
+#ifndef led_included
+#define led_included
+
+#include <msp430.h>
+
+#define LED_GREEN BIT0
+#define LED_RED BIT6
+#define LEDS (BIT0 | BIT6)
+
+void led_init();
+void led_update();
+
+extern unsigned char led_changed, green_on, red_on;
+
+#endif
