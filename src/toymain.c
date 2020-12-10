610ac8604e8120f6d829c534257839e23be3dd04
Addy <amgalvan@miners.utep.edu>
2020-12-09 23:52:09 -0700

    added draw diamon method and also added a switch cause to change the screen with buttons (testing)

--- src/toymain.c
+++ src/toymain.c
@@ -2,62 +2,93 @@
 #include <libTimer.h>
 #include "lcdutils.h"
 #include "lcddraw.h"
+#include "switches.h"
+#include "statemachine.h"
+#include "buzzer.h"
+#include "led.h"
 
-#define LED_GREEN BIT6
+//#define LED_GREEN BIT6
 
 short redrawScreen = 1;
-u_int fontFgColor = COLOR_GREEN;
+static short prev = 0;
+//u_int fontFgColor = COLOR_GREEN;
 
 long COLOR1;
 long COLOR2;
 
-void wdt_c_handler(){
-  static int count = 0;
-  count++;
-  if(count == 250){
-    count = 0;
-    redrawScreen = 1;
-  }
-}
+u_char centerWidth = (screenWidth/2) + 1;
+u_char centerHeight = (screenHeight/2) + 1;
 
-void main(){
-  P1DIR |= LED_GREEN;
-  P1OUT |= LED_GREEN;
-  configureClocks();
-  lcd_init();
+static u_char colorState = 0;
 
-  enableWDTInterrupts();
-  or_sr(0x8);
-
-  clearScreen(COLOR_BLACK);
-
-  u_char centerWidth = (screenWidth/2) + 1;
-  u_char centerHeight = (screenHeight/2) + 1;
-
-  static u_char colorState = 0;
-
+void draw_diamond(u_char col, u_char row, u_char size, u_int color1, u_int color2){
   while(1){
     if(redrawScreen){
       redrawScreen = 0;
 
       switch(colorState){
-      case 0: COLOR1 = COLOR_RED; COLOR2 = COLOR_WHITE; colorState = 1; break;
-      case 1: COLOR1 = COLOR_WHITE; COLOR2 = COLOR_RED; colorState = 0; break;
+      case 0: COLOR1 = color1; COLOR2 = color2; colorState = 1; break;
+      case 1: COLOR1 = color2; COLOR2 = color1; colorState = 0; break;
       }
-
-      for(u_char r = 0; r < 10; r++){
-	for(u_char c = 0; c <= r; c++){
-	  drawPixel(centerWidth-c, centerHeight-r-1, COLOR1);
-	  drawPixel(centerWidth+c, centerHeight-r-1, COLOR1);
-	  drawPixel(centerWidth-c, centerHeight+r-20, COLOR1);
-	  drawPixel(centerWidth+c, centerHeight+r-20, COLOR1);
+      
+      for(u_char r = 0; r < size; r++){
+	for(u_char c = 0; c < r; c++){
+	  drawPixel(col-c, row-r-1, COLOR1);
+	  drawPixel(col-c, row+r-(2*size), COLOR1);
+	  drawPixel(col+c, row-r-1, COLOR1);
+	  drawPixel(col+c, row+r-(2*size), COLOR1);
 	}
       }
+    }
+  }
+}
+
+void wdt_c_handler(){
+  static int count = 0;
+  static int count2 = 0;
+  if(button_state == 4){
+    if(++count == 250){
+      state_advance();
+      count = 0;
+      redrawScreen = 1;
+    }
+  }
+  if(++count2 == 125){
+    state_advance();
+    count2 = 0;
+  }
+}
+
+void main(){
+  led_init();
+  P1DIR |= LED_GREEN;
+  P1OUT |= LED_GREEN;
+  configureClocks();
+  lcd_init();
+  switch_init();
+  buzzer_init();
+  enableWDTInterrupts();
+  or_sr(0x8);
+
+  clearScreen(COLOR_YELLOW);
+
+  while(1){
+    if(redrawScreen){
+      redrawScreen = 0;
+
+      switch(button_state){
+      case 0: drawString5x7(screenWidth/2, screenHeight-10, "Adelyn", COLOR_BLUE, COLOR_YELLOW); break;
+      case 1: lcdState(COLOR_BLACK); break;
+      case 2: lcdState(COLOR_RED); break;
+      case 3: lcdState(COLOR_GREEN); break;
+      }
+    }
+  /*
       char* StrOnScreen = " adds";
 
       drawString5x7(centerWidth-(3*12), centerHeight+10, StrOnScreen, COLOR2, COLOR_BLACK);
-    }
-
+  */
+    
     P1OUT &= ~LED_GREEN;
     or_sr(0x10);
     P1OUT |= LED_GREEN;
