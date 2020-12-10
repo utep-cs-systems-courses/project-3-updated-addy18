0b3290b23882f55638f1ac7652c2c5f47c30301f
Addy <amgalvan@miners.utep.edu>
2020-12-10 02:47:01 -0700

    transfered my state_advance() to assembly code

---
+++ src/state_advance.s
@@ -0,0 +1,43 @@
+	.arch msp430g2553
+	.p2align 1,0
+	.text
+
+jt:
+	.word exit
+	.word case1
+	.word case2
+	.word case3
+	.word case4
+
+	.data
+	.extern button_state
+	.extern led_changed
+
+	.text
+	.global state_advance
+
+state_advance:
+	sub #2, r1		
+	mov #0 , 0(r1)		; changed = 0
+	cmp.b #5, &button_state
+	jhs exit
+	mov.b &button_state, r12
+	add.b r12, r12
+	mov jt(r12), r0
+
+case1:	call #toggle_red
+	mov.b r12, 0(r1)	; changed = toggle_red();
+	jmp exit
+case2:	call #state2
+	mov.b r12, 0(r1)	; changed = state2()
+	jmp exit
+case3:	call #state3
+	mov.b r12, 0(r1)	; changed = state3()
+	jmp exit
+case4:	call #state4
+	mov.b r12, 0(r1)
+	jmp exit
+exit:	mov.b 0(r1), &led_changed ;led_changed = changed
+	add #2, r1		  ; move stack pointer
+	call #led_update	  ; call led_update
+	pop r0			  ; return to caller 
