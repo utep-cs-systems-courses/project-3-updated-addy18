d1bfa89be016c1aaf768eb01264933fe4d0a1c2e
Addy <amgalvan@miners.utep.edu>
2020-12-11 06:35:23 -0700

    fixed my final state so it would only display 'merry christmas'

--- src/toymain.c
+++ src/toymain.c
@@ -7,55 +7,52 @@
 #include "buzzer.h"
 #include "led.h"
 
-//#define LED_GREEN BIT6
-
 short redrawScreen = 1;
-static short prev = 0;
-//u_int fontFgColor = COLOR_GREEN;
-
+u_int fontFgColor = COLOR_PURPLE;
 long COLOR1;
 long COLOR2;
+
+static int prev = 0;
 
 u_char centerWidth = (screenWidth/2) + 1;
 u_char centerHeight = (screenHeight/2) + 1;
 
 static u_char colorState = 0;
 
-void draw_diamond(u_char col, u_char row, u_char size, u_int color1, u_int color2){
-  while(1){
-    if(redrawScreen){
-      redrawScreen = 0;
-
-      switch(colorState){
-      case 0: COLOR1 = color1; COLOR2 = color2; colorState = 1; break;
-      case 1: COLOR1 = color2; COLOR2 = color1; colorState = 0; break;
-      }
-      
+void draw_diamond(u_char col, u_char row, u_char size, u_int color1){  
       for(u_char r = 0; r < size; r++){
 	for(u_char c = 0; c < r; c++){
-	  drawPixel(col-c, row-r-1, COLOR1);
-	  drawPixel(col-c, row+r-(2*size), COLOR1);
-	  drawPixel(col+c, row-r-1, COLOR1);
-	  drawPixel(col+c, row+r-(2*size), COLOR1);
+	  drawPixel(col-c, row-r-1, color1);
+	  drawPixel(col-c, row+r-(2*size), color1);
+	  drawPixel(col+c, row-r-1, color1);
+	  drawPixel(col+c, row+r-(2*size), color1);
 	}
       }
-    }
-  }
 }
 
 void wdt_c_handler(){
   static int count = 0;
   static int count2 = 0;
+  static int count3 = 0;
+  
   if(button_state == 4){
-    if(++count == 250){
+    if(++count == 250 && ++count3 == 250){
+      buzzer_advance();
       state_advance();
       count = 0;
+      count3 = 0;
       redrawScreen = 1;
     }
   }
   if(++count2 == 125){
     state_advance();
     count2 = 0;
+    redrawScreen = 1;
+  }
+  if(++count3 == 250){
+    count3 = 0;
+    fontFgColor = (fontFgColor == COLOR_PURPLE) ? COLOR_BLACK : COLOR_PURPLE;
+    redrawScreen = 1;
   }
 }
 
@@ -70,27 +67,47 @@ void main(){
   enableWDTInterrupts();
   or_sr(0x8);
 
-  clearScreen(COLOR_YELLOW);
+  clearScreen(COLOR_TURQUOISE);
 
   while(1){
     if(redrawScreen){
-      redrawScreen = 0;
-
       switch(button_state){
-      case 0: drawString5x7(screenWidth/2, screenHeight-10, "Adelyn", COLOR_BLUE, COLOR_YELLOW); break;
-      case 1: lcdState(COLOR_BLACK); break;
-      case 2: lcdState(COLOR_RED); break;
-      case 3: lcdState(COLOR_GREEN); break;
+      case 0: drawString5x7(screenWidth/2, screenHeight-15, "Adelyn", COLOR_BLUE, COLOR_TURQUOISE); break;
+      case 1:
+	//	clearScreen(COLOR_BLUE);
+	if(prev == 2){
+	  blankScreen(25, 30, 15);
+	}else if(prev == 3){
+	  blankScreen(45,20,20);
+	}
+	lcdState(COLOR_BLACK, 50, 50, 15); prev = 1; break;
+      case 2:
+	//	clearScreen(COLOR_TURQUOISE);
+	if(prev == 1){
+	  blankScreen(50,50,15);
+	}else if(prev == 3){
+	  blankScreen(45,20,20);
+	}
+	lcdState(COLOR_RED, 25, 30, 15); prev = 2; break;
+      case 3:
+	//	clearScreen(COLOR_PURPLE);
+	if(prev == 1){
+	  blankScreen(50,50,15);
+	}else if(prev == 2){
+	  blankScreen(25, 30, 15);
+	}
+	lcdState(COLOR_GREEN, 45, 20, 20); prev = 3; break;
+      case 4:
+	if(prev == 1){
+	  blankScreen(50,50,15);
+	}else if(prev == 2){
+	  blankScreen(25, 30, 15);
+	}else if(prev == 3){
+	  blankScreen(45, 20, 20);
+	}
+	//	 clearScreen(COLOR_BLACK);
+		drawString5x7(10,10,"Merry Christmas", fontFgColor, COLOR_TURQUOISE); prev = 4; break;
       }
     }
-  /*
-      char* StrOnScreen = " adds";
-
-      drawString5x7(centerWidth-(3*12), centerHeight+10, StrOnScreen, COLOR2, COLOR_BLACK);
-  */
-    
-    P1OUT &= ~LED_GREEN;
-    or_sr(0x10);
-    P1OUT |= LED_GREEN;
   }
 }
